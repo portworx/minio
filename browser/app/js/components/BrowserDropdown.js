fb50acbb415c0a0567d9cf22d3694bf2310892c1
paul <paul@portworx.com>
2017-03-20 13:36:49 -0700

    re-brand minio a bit

--- browser/app/js/components/BrowserDropdown.js
+++ browser/app/js/components/BrowserDropdown.js
@@ -27,22 +27,16 @@ let BrowserDropdown = ({fullScreenFunc, aboutFunc, settingsFunc, logoutFunc}) =>
         </Dropdown.Toggle>
         <Dropdown.Menu className="dropdown-menu-right">
           <li>
-            <a target="_blank" href="https://github.com/minio/minio">Github <i className="fa fa-github"></i></a>
-          </li>
-          <li>
             <a href="" onClick={ fullScreenFunc }>Fullscreen <i className="fa fa-expand"></i></a>
-          </li>
-          <li>
-            <a target="_blank" href="https://docs.minio.io/">Documentation <i className="fa fa-book"></i></a>
-          </li>
-          <li>
-            <a target="_blank" href="https://slack.minio.io">Ask for help <i className="fa fa-question-circle"></i></a>
           </li>
           <li>
             <a href="" onClick={ aboutFunc }>About <i className="fa fa-info-circle"></i></a>
           </li>
           <li>
             <a href="" onClick={ settingsFunc }>Settings <i className="fa fa-cog"></i></a>
+          </li>
+          <li>
+            <a target="_blank" href="http://slack.portworx.com">Ask for help <i className="fa fa-question-circle"></i></a>
           </li>
           <li>
             <a href="" onClick={ logoutFunc }>Sign Out <i className="fa fa-sign-out"></i></a>
